65b5e1871d280185c536fa46ddc45a48398e0539
cesarpps <mendescesar309@gmail.com>
2025-11-21 21:00:56 +0000

    Ajout modal edition/suppresion

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,12 +4,11 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html", []),
     new Route("/galerie", "La galerie", "/pages/galerie.html", [], "/js/galerie.js"),
-    new Route("/signin", "Connexion", "/pages/auth/signin.html", ["disconnected"], "/js/auth/signin.js"),
-    new Route("/signup", "Inscription", "/pages/auth/signup.html", ["disconnected"], "/js/auth/signup.js"),
-    new Route("/account", "Mon compte", "/pages/auth/account.html", ["client", "admin"]),
-    new Route("/editPassword", "Changement de mot de passe", "/pages/auth/editPassword.html", ["client", "admin"]),
-    new Route("/allResa", "Vos réservations", "/pages/reservations/allResa.html", ["client"]),
-    new Route("/reserver", "Réserver", "/pages/reservations/reserver.html", ["client"]),
+    new Route("/singin", "Connexion", "/pages/singin.html", [], "/js/galerie.js"),
+    new Route("/singup", "Inscription", "/pages/singup.html", [], "/js/galerie.js"),
+    new Route("/account", "Mon compte", "/pages/account.html", [], "/js/galerie.js"),
+    new Route("/editpassword", "Mot de passe", "/pages/editpassword.html", [], "/js/galerie.js"),
+
 ];
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Quai Antique";
